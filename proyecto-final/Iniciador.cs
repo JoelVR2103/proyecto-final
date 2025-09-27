d59fb5e0837f75565ffbcd5165921fae7991fead
JoelVR1307 <cristofer.villa.1307@gmail.com>
2025-09-26 21:21:49 -0500

    Inicio de planteamiento proyecto, creando estructura de bibliotecas

---
+++ proyecto-final/Iniciador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final
+{
+    class Iniciador
+    {
+        public static void iniciador()
+        {
+            Console.WriteLine("Hola porfavor ingresa tu dni");
+            int dniIngresado = Convert.ToInt32(Console.ReadLine());
+            int dni = 74758787;
+
+            string nombres = "Pedro";
+            string apellidos = "Perez";
+
+
+            if (dniIngresado == dni)
+            {
+                Console.WriteLine("Bienvenido " + nombres + " " + apellidos);
+                Console.WriteLine("Quieres actualizar tus datos? (si/no)");
+            }
+            else
+            {
+                Console.WriteLine("No estas registrado, porfavor registrate");
+                Console.WriteLine("Ingresa tus nombres");
+            }
+        } 
+    }
+}
---
+++ proyecto-final/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Iniciador.iniciador();
+            /// lucinda
+            /// pedido de dni
+            /// si existe deberia darte la bienvenida
+            /// te pregunta si quieres actualizar tus datos
+            /// 
+            /// sino deberia pedirte que te registres
+            /// al registrarte pedir nombres, apellidos, edad, sexo, dni, telefono, email, direccion, grado.
+            /// 
+            /// una vez registrado deberia darte la bienvenida
+            /// 
+            /// -----------------------------
+            /// andree
+            ///
+            /// elegir el grado de matricula
+            /// elefir el turno mañana o tarde
+            ///
+            /// dos opciones para elegir materias o el sistema elige por vos
+            /// 
+            /// elegir las materias o cursos - validar no repetir cursos. como max 6 materias
+            /// 
+            /// sino el sistema elige por vos 6 cursos
+            /// 
+            /// -----------------------------
+            /// yacson
+            /// 
+            /// luego de elegir las materias deberia mostrarte un resumen de tu matricula
+            /// preguntar si esta correcto
+            /// si el usuario se equivoco deberia darle la opcion de volver a elegir las materias
+            /// si todo esta bien deberia confirmar la matricula
+            /// retornar un mensaje de confirmacion de matricula
+            /// imprime matricula datos
+
+
+            /// vista - modelo - controlador joel, encargado de distribuir las tareas, funciones en el controlador y modelo bd json para no perder datos.
+        }
+    }
+}
